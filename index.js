bf46f16eb306fec0789f92d7dc2779c0cb24c486
Ahmad M. <muhammadahmad476@gmail.com>
2024-08-17 01:59:10 +0500

    new feaure

--- index.js
+++ index.js
@@ -11,24 +11,53 @@ app.get('/redirect', (req, res) => {
     // URL to redirect to (in this case, Microsoft)
     const targetUrl = 'https://www.microsoft.com';
 
-    // HTML with meta refresh and JavaScript redirect
-    const html = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <meta http-equiv="refresh" content="0;url=${targetUrl}">
-      <script>
-        window.location.href = "${targetUrl}";
-      </script>
-    </head>
-    <body>
-      <p>Redirecting to ${targetUrl}...</p>
-      <p>If you are not redirected, <a href="${targetUrl}">click here</a>.</p>
-    </body>
-    </html>
-  `;
+    // Detect if the user is on a mobile device
+    const userAgent = req.headers['user-agent'] || '';
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
 
-    res.send(html);
+    if (isMobile) {
+        // Mobile-specific solution
+        const html = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script>
+          (function() {
+            var targetUrl = "${targetUrl}";
+            var openedWindow;
+            
+            function tryOpen() {
+              openedWindow = window.open(targetUrl, '_system');
+              if (!openedWindow || openedWindow.closed || typeof openedWindow.closed == 'undefined') {
+                window.location.href = targetUrl;
+              }
+            }
+
+            // Try multiple times
+            tryOpen();
+            setTimeout(tryOpen, 100);
+            setTimeout(tryOpen, 500);
+            setTimeout(tryOpen, 1000);
+
+            // Fallback
+            setTimeout(function() {
+              window.location.href = targetUrl;
+            }, 2000);
+          })();
+        </script>
+      </head>
+      <body>
+        <p>Redirecting to ${targetUrl}...</p>
+        <p>If you are not redirected, <a href="${targetUrl}">click here</a>.</p>
+      </body>
+      </html>
+    `;
+        res.send(html);
+    } else {
+        // Non-mobile solution
+        res.redirect(targetUrl);
+    }
 });
 app.listen(10000, () => {
     console.log(`Server listening at http://localhost:${10000}`);
