fde481c5b86f5bb691e30e62367a18a3d13cc5ee
Ahmad M. <muhammadahmad476@gmail.com>
2024-08-17 01:29:13 +0500

    update port

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.get("/health-check", (req, res) => {
     res.send("OK");
 });
 
-app.listen(3000, () => {
-    console.log(`listening on port 3000`);
+app.listen(10000, () => {
+    console.log(`listening on port 10000`);
 });
