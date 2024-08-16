497565761b71ee3e98ddf485268d83c71b971a88
Ahmad M. <muhammadahmad476@gmail.com>
2024-08-17 01:15:36 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import open from "open";
+
+
+const app = express();
+
+app.get("/", async (req, res) => {
+    await open("https://google.com");
+});
+
+app.get("/health-check", (req, res) => {
+    res.send("OK");
+});
+
+app.listen(3000, () => {
+    console.log(`listening on port 3000`);
+});
